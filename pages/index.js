5f3cb4b643a6e36f0f3b85fd5b55bf3639d750b4
Joseph Ryan IV <joseph.ryan.iv@gmail.com>
2022-10-13 20:58:43 -0700

    delete import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
-import homepageImage from '../public/lake2.jpg'
 
 export default function Home() {
     return (
